b5b10a8413f60b11ede0067830ca3e416bad6395
Timur Kostenko <ktim8168@mail.ru>
2019-01-27 19:59:26 +0300

    Refactoring

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -1,34 +1,13 @@
-/* eslint-disable no-unused-vars */
-// http://idangero.us/swiper/#.WcIu5oy0OHs
-import * as Swiper from 'swiper/dist/js/swiper';
-
+/* eslint-disable */
 const $ = window.$;
 
 export default function slider() {
-  const mySlider = new Swiper('.js-slider', {
-    loop: true,
-    speed: 700,
-    autoplay: {
-      delay: 2000,
-    },
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.slider__button_next',
-      prevEl: '.slider__button_prev',
-    },
-    pagination: {
-      el: '.slider__dots',
-      clickable: true,
-      paginationClickableClass: 'slider__dots_clickable',
-      bulletClass: 'slider__dot',
-      bulletActiveClass: 'active',
-    },
-    roundLengths: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-    },
+  $('.slider-nav__button').on('click', function() {
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+    if ($(this).index() != $('.section__bg-img.is-active').index()) {
+      $('.section__bg-img.is-active').removeClass('is-active').siblings('.section__bg-img').addClass('is-active');
+      $('.greeting.is-active').removeClass('is-active').siblings('.greeting').addClass('is-active');
+    }
   });
 }
-/* eslint-enable no-unused-vars */
+/* eslint-enable */
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import objectFitImages from 'object-fit-images';
 import anchor from '../blocks/js-functions/anchor';
+import slider from '../blocks/slider/slider';
 import './globalOptions';
 import scrollanimation from '../blocks/js-functions/scrollanimation';
 import scrollbar from '../blocks/scrollbar/scrollbar';
-import mainSlider from '../components/main/main-slider';
 
 const $ = window.$;
 
@@ -12,7 +12,7 @@ $(() => {
   svg4everybody();
   objectFitImages();
   anchor();
+  slider();
   scrollbar();
-  mainSlider();
   scrollanimation();
 });
